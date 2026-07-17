5de9645923809d4192dd045929d8d87800179d5f
SubhanEfendiyev200 <subhan.efendiyevv@gmail.com>
2026-07-17 15:37:35 +0400

    Compleated main section without slider,and started footer

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,1 +1,15 @@
 "use strict"
+
+const emailInput = document.getElementById('emailInput');
+const submitBtn = document.getElementById('submitBtn');
+
+emailInput.addEventListener('focus', function () {
+    submitBtn.classList.add('active');
+});
+
+emailInput.addEventListener('blur', function () {
+    submitBtn.classList.remove('active');
+});
+submitBtn.addEventListener('click', function () {
+    document.querySelector('.signupMessage').textContent = "Check your email for a confirmation message. Thanks for subscribing!";
+});
